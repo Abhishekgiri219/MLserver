a3a255942b4f6d505bf59f076d6edf8512a061e9
Abhishek Kumar <abhishekgiri219@gmail.com>
2022-12-04 03:45:35 +0530

    initial commit, working code

--- iot/settings.py
+++ iot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-faw_q_sd%p+ss9q1h8l@ing@15aq_8hst=*@^0uuc%nse2u^ll
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
